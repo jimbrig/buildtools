38043326f1e73f070cf97c2ff220f2d364da2fee
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-06-08 20:01:24 -0400

    feat: add guide and pkgdown funcs

---
+++ R/open_guide.R
@@ -0,0 +1,18 @@
+#' Open UserGuide of the package
+#'
+#' @param ext extension of the book to open: 'html', 'pdf'
+#'
+#' @importFrom utils browseURL
+#'
+#' @export
+open_guide <- function(ext = "html") {
+    if (ext == "html") {
+    guide_path <- system.file('report/_book/index.html', package = 'buildtools')
+    } else if (ext == "pdf") {
+    guide_path <- system.file('report/_book/report.pdf', package = 'buildtools')
+    } else {
+    guide_path <- system.file(paste0("report/_book/report.", ext[1]), package = 'buildtools')
+    }
+
+  browseURL(paste0('file://', guide_path))
+}
