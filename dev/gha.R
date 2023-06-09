35d89fb0308cfefe108a8a002eb1e285878bbcaf
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-06-08 20:28:04 -0400

    feat: dev scripts

--- dev/gha.R
+++ dev/gha.R
@@ -10,3 +10,15 @@ usethis::use_github_action("check-release")
 usethis::use_github_action("document")
 usethis::use_github_action("lint")
 usethis::use_github_action("style")
+
+usethis::use_github_actions_badge(name = "check.yml")
+
+workflows <- fs::dir_ls(".github/workflows") |>
+    basename() |>
+    as.character()
+
+workflows |> purrr::walk(
+    usethis::use_github_actions_badge
+)
+
+devtools::build_readme()
