2395a8033f93e4d8078dcdc13d312d4b629a9c3c
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-06-08 20:49:55 -0400

    feat: packge enhancements

---
+++ R/globals.R
@@ -0,0 +1,12 @@
+utils::globalVariables(
+    c(
+        "maintainer",
+        "Repository",
+        "Package",
+        "Version",
+        "GithubUsername",
+        "GithubSHA1",
+        "install_cmd",
+        "."
+    )
+)
